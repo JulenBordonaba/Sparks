c8610bd7502a37e25d2219ed5277bf3c7bada774
Julen Bordonaba <48485297+JulenBordonaba@users.noreply.github.com>
2020-01-30 12:04:48 +0100

    barra de vida

---
+++ 3º Motores Práctica 1º cuatrimestre/Assets/_MisAssets/Scripts/HealthHUD.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthHUD : MonoBehaviour
+{
+    public PlayerController player;
+    public Image healthbar;
+
+    private float startPosition;
+    private float minPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        minPosition = -healthbar.rectTransform.rect.width;
+        startPosition = healthbar.rectTransform.localPosition.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = healthbar.rectTransform.localPosition;
+        pos.x = Mathf.Lerp(minPosition, startPosition, healthProportion);
+        healthbar.rectTransform.localPosition = pos;
+    }
+
+
+
+    public float healthProportion
+    {
+        get { return player.currentHealth / player.maxHealth; }
+    }
+
+}
